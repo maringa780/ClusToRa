981ca56c1448749008082970545ec6bb55b679c2
maringa780 <68044059+maringa780@users.noreply.github.com>
2026-05-23 17:41:46 -0400

    Delete app/test.m

--- app/test.m
+++
@@ -1,9 +0,0 @@
-%[fList, pList] = matlab.codetools.requiredFilesAndProducts('ClusToRa_GUI.mlapp');
-% Get folder where startup.m lives
-rootFolder = fileparts(mfilename('fullpath'));
-
-% Add all subfolders
-addpath(genpath(rootFolder));
-
-%Start ClusToRa
-ClusToRa_GUI
